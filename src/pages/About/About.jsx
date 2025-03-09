c4a0e0db816c7158715cdd2853d2770647001b4f
Kassandra Peasley <inspiredby.you@outlook.com>
2025-03-08 20:52:19 -0600

    Updateed import paths to absolute paths for consistent deployment

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 // Import dependencies and styles
-import './About.css';
-import profileImage from "../../assets/bleh.jpg";
+import '/src/pages/About/About.css';
+import profileImage from '/src/assets/bleh.jpg';
 
 function About() {
     return (
@@ -14,8 +14,8 @@ function About() {
                     Becoming more is simply an act of doing more. Let's do better together.
                 </p>
                 <p>
-                Welcome! I'm Kassandra, a full-stack web developer passionate about 
-                creating responsive and user-friendly applications. I strive to bring a unique perspective to every project.
+                    Welcome! I'm Kassandra, a full-stack web developer passionate about 
+                    creating responsive and user-friendly applications. I strive to bring a unique perspective to every project.
                 </p>
             </div>
         </section>
